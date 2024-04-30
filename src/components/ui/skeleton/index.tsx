5bfe398adfb444d20bb0dbd900ca26b6d306cbcc
Erfan <erfansormi97@gmail.com>
2024-04-30 14:47:21 +0330

    created skeleton component

---
+++ src/components/ui/skeleton/index.tsx
@@ -0,0 +1,30 @@
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "../../../lib/utils";
+import styles from "./skeleton.module.css";
+
+type SkeletonVariantProps = VariantProps<typeof skeletonVariants>;
+const skeletonVariants = cva("block", {
+  variants: {
+    rounded: { full: "rounded-full", lg: "rounded-lg" },
+  },
+  defaultVariants: {
+    rounded: "lg",
+  },
+});
+
+interface Props extends SkeletonVariantProps {
+  className?: string;
+  width?: number | string;
+  height?: number | string;
+}
+
+const Skeleton = ({ rounded, className, width, height }: Props) => {
+  return (
+    <div
+      style={{ width, height }}
+      className={cn(styles.skeleton_box, skeletonVariants({ rounded, className }))}
+    ></div>
+  );
+};
+
+export default Skeleton;
