9b4da74ddc5c4e1d48811798c0849a7f024377f1
Erfan <erfansormi97@gmail.com>
2024-04-30 14:45:38 +0330

    Update error-message.ts

--- src/utils/error-message.ts
+++ src/utils/error-message.ts
@@ -3,4 +3,6 @@ export const errorMessages = Object.freeze({
   invalidFormat: (name: string) => `فرمت ${name} وارد شده صحیح نمی‌باشد`,
   minLength: (name: string, min: number) => `${name} نمی‌تواند کمتر از ${min} کاراکتر داشته باشد`,
   maxLength: (name: string, max: number) => `${name} نمی‌تواند بیشتر از ${max} کاراکتر داشته باشد`,
+  min: (name: string, min: number) => `${name} نمی‌تواند کمتر از ${min} باشد`,
+  max: (name: string, max: number) => `${name} نمی‌تواند بیشتر از ${max} باشد`,
 });
